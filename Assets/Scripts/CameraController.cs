9899f9201540b281830da01ee0005c9b62a300bd
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-09-02 15:20:57 +1000

    basic controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private float smoothTime = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPos = target.position + CalcDesiredOffset();
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothTime);
+
+        transform.position = smoothedPos;
+        transform.LookAt(target);
+    }
+    private Vector3 CalcDesiredOffset()
+    {
+        Vector3 desired = Vector3.zero;
+        desired += target.right * offset.x;
+        desired += target.up * offset.y;
+        desired += target.forward * offset.z;
+        return desired;
+    }
+}
---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
+    private float topSpeed = 10;
+    [SerializeField]
+    private float turnSpeed = 1;
+
+    private Rigidbody rb;
+    private Vector3 velocity;
+    private float speed = 0;
+    private Gyroscope gyro;
+    private float rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        gyro = Input.gyro;
+        gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        rotation += gyro.gravity.z;
+        transform.rotation = Quaternion.AngleAxis(rotation, Vector3.up);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = velocity;
+        rb.AddForceAtPosition(new Vector3(0, 0, acceleration), transform.position, ForceMode.Impulse);
+    }
+}
